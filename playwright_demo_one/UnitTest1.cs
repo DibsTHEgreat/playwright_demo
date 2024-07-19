4878c9e6e7f5a91bed341ed67a022460e091925e
Divya.Pateliya <Divya.Pateliya@arcurve.com>
2024-07-19 11:00:44 -0600

    Add project files.

---
+++ playwright_demo_one/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace playwright_demo_one
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
